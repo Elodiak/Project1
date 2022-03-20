c86e539f14fea362961c848a57cbffe057f58bb6
Anastasiia K <elodiak@ya.ru>
2022-03-20 19:09:44 +0500

    encode+decode

--- src/main/java/ru/javarush/kozhevnikova/cryptoalalizer/OneMainClass.java
+++ src/main/java/ru/javarush/kozhevnikova/cryptoalalizer/OneMainClass.java
@@ -2,63 +2,73 @@ package ru.javarush.kozhevnikova.cryptoalalizer;
 
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class OneMainClass {
 
+    public static final String TXT = System.getProperty("user.dir")+File.separator+"text"+File.separator;
 
-    public static final char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            ' ', '.', ',', '!', '?'};
 
-    public static void encryption(char c, int key) {
+    public static void main(String[] args) throws IOException {
 
-        for (int i = 0; i < alphabet.length; i++) {
-            if (alphabet[i] == c) {
-                if (i + key >= alphabet.length) {
-                    int num2 = alphabet.length - i; // к-во цифр от символа до конца массива
-                    int num = key - num2; // к-во которое надо будет добавить
-                    i = 0;
-                    System.out.print(alphabet[i + num]);
-                    break;
-                } else {
-                    System.out.print(alphabet[i + key]);
-                    break;
-                }
+        System.out.println(TXT);
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введи \"1\" для шифрования, \"2\" для дешифрования, \"3\" для bruteforce");
+        int number = scanner.nextInt();
+
+        System.out.println("Введи путь к файлу:");
+
+        String scanSource = scanner.next();
+
+        //String scanSource = "/Users/nasty/Desktop/text.txt";
+
+
+        if (scanSource.equals("exit")) {
+            File source = new File(scanSource);
+            if (!source.exists()) {
+                throw new AppException("File not found");
             }
         }
 
-    }
+        System.out.println("Введи путь к файлу либо нажми \"exit\" для того что бы использовать файл по умолчанию");
 
+            String scanDest = scanner.next();
 
-//    public static void decryption(char c, int key) {
-//
-//    }
-
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        //String scanSource = scanner.nextLine();
-        //String scanDestination = scanner.nextLine();
-        //System.out.println(alphabet.length);
-
-        try (FileReader source = new FileReader("/Users/nasty/Desktop/text.txt");
-             FileWriter destination = new FileWriter("/Users/nasty/Desktop/somethingtext.txt")) {
-
-            int key = scanner.nextInt();
-
-            char[] buffer = new char[65536];
-
-            while (source.ready()) {
-                int real = source.read(buffer);
-                for (char c : buffer) {
-                    encryption(c, key);
-
-                }
-
-                destination.write(buffer, 0, real);
+            if(scanDest.equals("exit")) {
+                scanDest = TXT + "text.txt";
+                Files.createDirectory(Path.of(TXT));
+                Files.createFile(Path.of(scanDest));
             }
 
+        System.out.println(scanDest);
+
+        try (FileReader source = new FileReader(scanSource);
+             FileWriter destination = new FileWriter(scanDest)) {
+
+//        try (FileReader source = new FileReader("/Users/nasty/Desktop/Project/Project1/src/main/java/ru/javarush/kozhevnikova/cryptoalalizer/TextOne.txt");
+//             FileWriter destination = new FileWriter("/Users/nasty/Desktop/text3.txt")) {
+
+            System.out.println("key");
+            int key = scanner.nextInt(); //от 0 до длины алфавита
+
+            if (key > Alphabet.alphabet.length) {
+                key = key % Alphabet.alphabet.length;
+            }
+
+            switch (number) {
+                case (1):
+                    Encode.encode(source, destination, key);
+                    break;
+                case (2):
+                    Decode.decode(source, destination, key);
+                    break;
+                case (3):
+                    break;
+            }
 
         }
     }
